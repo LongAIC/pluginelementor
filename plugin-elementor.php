de834285d3478c0bb0bfd541604bb73f05122745
trungnv210 <trungvn@techverest.com>
2023-02-07 15:20:35 +0700

    update name plugin

--- plugin-elementor.php
+++ plugin-elementor.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Elementor Test Addon
+ * Plugin Name: Ftech Plugin Elementor
  * Description: Custom Elementor addon.
  * Plugin URI:  https://elementor.com/
  * Version:     1.0.0
@@ -16,13 +16,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-function elementor_test_addon() {
+function ftech_plugin_elementor() {
 
 	// Load plugin file
 	require_once( __DIR__ . '/includes/plugin.php' );
 
 	// Run the plugin
-	\Elementor_Test_Addon\Plugin::instance();
+	\FtechPluginElementor\Plugin::instance();
 
 }
-add_action( 'plugins_loaded', 'elementor_test_addon' );
+add_action( 'plugins_loaded', 'ftech_plugin_elementor' );
