832e96f9f822ed42cdf2106f9c7b2172aba174e3
lelong <lelong24072306@gmail.com>
2023-02-07 15:36:30 +0700

    Update plugin

--- includes/plugin.php
+++ includes/plugin.php
@@ -2,7 +2,7 @@
 namespace FtechPluginElementor;
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly.
 }
 
 /**
@@ -14,265 +14,263 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 final class Plugin {
 
-	/**
-	 * Addon Version
-	 *
-	 * @since 1.0.0
-	 * @var string The addon version.
-	 */
-	const VERSION = '1.0.0';
+    /**
+     * Addon Version
+     *
+     * @since 1.0.0
+     * @var string The addon version.
+     */
+    const VERSION = '1.0.0';
 
-	/**
-	 * Minimum Elementor Version
-	 *
-	 * @since 1.0.0
-	 * @var string Minimum Elementor version required to run the addon.
-	 */
-	const MINIMUM_ELEMENTOR_VERSION = '3.7.0';
+    /**
+     * Minimum Elementor Version
+     *
+     * @since 1.0.0
+     * @var string Minimum Elementor version required to run the addon.
+     */
+    const MINIMUM_ELEMENTOR_VERSION = '3.7.0';
 
-	/**
-	 * Minimum PHP Version
-	 *
-	 * @since 1.0.0
-	 * @var string Minimum PHP version required to run the addon.
-	 */
-	const MINIMUM_PHP_VERSION = '7.3';
+    /**
+     * Minimum PHP Version
+     *
+     * @since 1.0.0
+     * @var string Minimum PHP version required to run the addon.
+     */
+    const MINIMUM_PHP_VERSION = '7.3';
 
-	/**
-	 * Instance
-	 *
-	 * @since 1.0.0
-	 * @access private
-	 * @static
-	 * @var \Elementor_Test_Addon\Plugin The single instance of the class.
-	 */
-	private static $_instance = null;
+    /**
+     * Instance
+     *
+     * @since 1.0.0
+     * @access private
+     * @static
+     * @var \Elementor_Test_Addon\Plugin The single instance of the class.
+     */
+    private static $_instance = null;
 
-	/**
-	 * Instance
-	 *
-	 * Ensures only one instance of the class is loaded or can be loaded.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 * @static
-	 * @return \Elementor_Test_Addon\Plugin An instance of the class.
-	 */
-	public static function instance() {
+    /**
+     * Instance
+     *
+     * Ensures only one instance of the class is loaded or can be loaded.
+     *
+     * @since 1.0.0
+     * @access public
+     * @static
+     * @return \Elementor_Test_Addon\Plugin An instance of the class.
+     */
+    public static function instance() {
 
-		if ( is_null( self::$_instance ) ) {
-			self::$_instance = new self();
-		}
-		return self::$_instance;
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
 
-	}
+    }
 
-	/**
-	 * Constructor
-	 *
-	 * Perform some compatibility checks to make sure basic requirements are meet.
-	 * If all compatibility checks pass, initialize the functionality.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function __construct() {
+    /**
+     * Constructor
+     *
+     * Perform some compatibility checks to make sure basic requirements are meet.
+     * If all compatibility checks pass, initialize the functionality.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function __construct() {
 
-		if ( $this->is_compatible() ) {
-			add_action( 'elementor/init', [ $this, 'init' ] );
-		}
+        if ( $this->is_compatible() ) {
+            add_action( 'elementor/init', [ $this, 'init' ] );
+        }
 
-	}
+    }
 
-	/**
-	 * Compatibility Checks
-	 *
-	 * Checks whether the site meets the addon requirement.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function is_compatible() {
+    /**
+     * Compatibility Checks
+     *
+     * Checks whether the site meets the addon requirement.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function is_compatible() {
 
-		// Check if Elementor installed and activated
-		if ( ! did_action( 'elementor/loaded' ) ) {
-			add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
-			return false;
-		}
+        // Check if Elementor installed and activated
+        if ( ! did_action( 'elementor/loaded' ) ) {
+            add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
+            return false;
+        }
 
-		// Check for required Elementor version
-		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
-			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
-			return false;
-		}
+        // Check for required Elementor version
+        if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+            add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
+            return false;
+        }
 
-		// Check for required PHP version
-		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
-			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
-			return false;
-		}
+        // Check for required PHP version
+        if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+            add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
+            return false;
+        }
 
-		return true;
+        return true;
 
-	}
+    }
 
-	/**
-	 * Admin notice
-	 *
-	 * Warning when the site doesn't have Elementor installed or activated.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function admin_notice_missing_main_plugin() {
+    /**
+     * Admin notice
+     *
+     * Warning when the site doesn't have Elementor installed or activated.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function admin_notice_missing_main_plugin() {
 
-		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+        if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
 
-		$message = sprintf(
-			/* translators: 1: Plugin name 2: Elementor */
-			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'elementor-test-addon' ),
-			'<strong>' . esc_html__( 'Elementor Test Addon', 'elementor-test-addon' ) . '</strong>',
-			'<strong>' . esc_html__( 'Elementor', 'elementor-test-addon' ) . '</strong>'
-		);
+        $message = sprintf(
+        /* translators: 1: Plugin name 2: Elementor */
+            esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'elementor-test-addon' ),
+            '<strong>' . esc_html__( 'Ftech Plugin Elementor', '' ) . '</strong>',
+            '<strong>' . esc_html__( 'Elementor', '' ) . '</strong>'
+        );
+        printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
 
-		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+    }
 
-	}
+    /**
+     * Admin notice
+     *
+     * Warning when the site doesn't have a minimum required Elementor version.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function admin_notice_minimum_elementor_version() {
 
-	/**
-	 * Admin notice
-	 *
-	 * Warning when the site doesn't have a minimum required Elementor version.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function admin_notice_minimum_elementor_version() {
+        if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
 
-		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+        $message = sprintf(
+        /* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+            esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-test-addon' ),
+            '<strong>' . esc_html__( 'Elementor Test Addon', 'elementor-test-addon' ) . '</strong>',
+            '<strong>' . esc_html__( 'Elementor', 'elementor-test-addon' ) . '</strong>',
+            self::MINIMUM_ELEMENTOR_VERSION
+        );
 
-		$message = sprintf(
-			/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
-			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-test-addon' ),
-			'<strong>' . esc_html__( 'Elementor Test Addon', 'elementor-test-addon' ) . '</strong>',
-			'<strong>' . esc_html__( 'Elementor', 'elementor-test-addon' ) . '</strong>',
-			 self::MINIMUM_ELEMENTOR_VERSION
-		);
+        printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
 
-		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+    }
 
-	}
+    /**
+     * Admin notice
+     *
+     * Warning when the site doesn't have a minimum required PHP version.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function admin_notice_minimum_php_version() {
 
-	/**
-	 * Admin notice
-	 *
-	 * Warning when the site doesn't have a minimum required PHP version.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function admin_notice_minimum_php_version() {
+        if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
 
-		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+        $message = sprintf(
+        /* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+            esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-test-addon' ),
+            '<strong>' . esc_html__( 'Ftech Plugin Elementor', '' ) . '</strong>',
+            '<strong>' . esc_html__( 'PHP', 'elementor-test-addon' ) . '</strong>',
+            self::MINIMUM_PHP_VERSION
+        );
 
-		$message = sprintf(
-			/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
-			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-test-addon' ),
-			'<strong>' . esc_html__( 'Elementor Test Addon', 'elementor-test-addon' ) . '</strong>',
-			'<strong>' . esc_html__( 'PHP', 'elementor-test-addon' ) . '</strong>',
-			 self::MINIMUM_PHP_VERSION
-		);
+        printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
 
-		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+    }
 
-	}
+    /**
+     * Initialize
+     *
+     * Load the addons functionality only after Elementor is initialized.
+     *
+     * Fired by `elementor/init` action hook.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function init() {
 
-	/**
-	 * Initialize
-	 *
-	 * Load the addons functionality only after Elementor is initialized.
-	 *
-	 * Fired by `elementor/init` action hook.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function init() {
+        add_action( 'elementor/widgets/register', [ $this, 'register_widgets' ] );
+        add_action( 'elementor/controls/register', [ $this, 'register_controls' ] );
+        add_action( 'wp_enqueue_scripts', [ $this, 'register_widget_styles' ] );
+        // add_action( 'elementor/widgets/widgets_registered', function() {
+        // 	if ( class_exists( 'Elementor\Widget_Base' ) ) {
+        // 		add_action( 'wp_enqueue_scripts', [$this, 'my_library_enqueue'] );
+        // 	}
+        // });
+        add_action( 'elementor/frontend/after_enqueue_styles', [$this, 'register_elementor_widget_styles'] );
+        add_action( 'elementor/frontend/after_register_styles', [$this, 'enqueue_elementor_widget_styles'] );
+        add_action( 'wp_enqueue_scripts', [$this, 'register_widget_scripts'] );
 
-		add_action( 'elementor/widgets/register', [ $this, 'register_widgets' ] );
-		add_action( 'elementor/controls/register', [ $this, 'register_controls' ] );
-		add_action( 'wp_enqueue_scripts', [ $this, 'register_widget_styles' ] );
-		// add_action( 'elementor/widgets/widgets_registered', function() {
-		// 	if ( class_exists( 'Elementor\Widget_Base' ) ) {
-		// 		add_action( 'wp_enqueue_scripts', [$this, 'my_library_enqueue'] );
-		// 	}
-		// });
-		add_action( 'elementor/frontend/after_enqueue_styles', [$this, 'register_elementor_widget_styles'] );
-		add_action( 'elementor/frontend/after_register_styles', [$this, 'enqueue_elementor_widget_styles'] );
-		add_action( 'wp_enqueue_scripts', [$this, 'register_widget_scripts'] );
+    }
 
-	}
-
-	/**
-	 * Register Widgets
-	 *
-	 * Load widgets files and register new Elementor widgets.
-	 *
-	 * Fired by `elementor/widgets/register` action hook.
-	 *
-	 * @param \Elementor\Widgets_Manager $widgets_manager Elementor widgets manager.
-	 */
-	public function register_widgets( $widgets_manager ) {
-		require_once(__DIR__ . '/modules/popup-image/widgets/popup-image.php');
+    /**
+     * Register Widgets
+     *
+     * Load widgets files and register new Elementor widgets.
+     *
+     * Fired by `elementor/widgets/register` action hook.
+     *
+     * @param \Elementor\Widgets_Manager $widgets_manager Elementor widgets manager.
+     */
+    public function register_widgets( $widgets_manager ) {
+        require_once(__DIR__ . '/modules/popup-image/widgets/popup-image.php');
         require_once(__DIR__ . '/widgets/widget-2.php');
         require_once (__DIR__. '/widgets/slides.php');
 
 //		$widgets_manager->register( new \Popup_Image_123() );
         $widgets_manager->register( new \Widget_2() );
         $widgets_manager->register( new \Slides() );
-	}
+    }
 
-	/**
-	 * Register Controls
-	 *
-	 * Load controls files and register new Elementor controls.
-	 *
-	 * Fired by `elementor/controls/register` action hook.
-	 *
-	 * @param \Elementor\Controls_Manager $controls_manager Elementor controls manager.
-	 */
-	public function register_controls( $controls_manager ) {
+    /**
+     * Register Controls
+     *
+     * Load controls files and register new Elementor controls.
+     *
+     * Fired by `elementor/controls/register` action hook.
+     *
+     * @param \Elementor\Controls_Manager $controls_manager Elementor controls manager.
+     */
+    public function register_controls( $controls_manager ) {
 
-		require_once( __DIR__ . '/controls/control-1.php' );
-		// require_once( __DIR__ . '/includes/controls/control-2.php' );
+        require_once( __DIR__ . '/controls/control-1.php' );
+        // require_once( __DIR__ . '/includes/controls/control-2.php' );
 
-		$controls_manager->register( new \Control_1() );
-		// $controls_manager->register( new Control_2() );
+        $controls_manager->register( new \Control_1() );
+        // $controls_manager->register( new Control_2() );
 
-	}
+    }
 
- 	public function register_widget_styles() {
-		wp_register_style( 'widgets-2-style', plugins_url( '/assets/css/widgets-2-style.css', __FILE__ ) );
-	}
+    public function register_widget_styles() {
+        wp_register_style( 'widgets-2-style', plugins_url( '/assets/css/widgets-2-style.css', __FILE__ ) );
+    }
 
-	function register_elementor_widget_styles() {
-		wp_register_style( 'widgets-2-style', plugin_dir_url( __FILE__ ) . '/assets/css/widgets-2-style.css' );
-		wp_register_style( 'mag-popup', plugin_dir_url( __FILE__ ) . '/assets/Popup/lib/magnific-popup.css' );
-	}
+    function register_elementor_widget_styles() {
+        wp_register_style( 'widgets-2-style', plugin_dir_url( __FILE__ ) . '/assets/css/widgets-2-style.css' );
+        wp_register_style( 'mag-popup', plugin_dir_url( __FILE__ ) . '/assets/Popup/lib/magnific-popup.css' );
+    }
 
-	function enqueue_elementor_widget_styles() {
-		wp_enqueue_style( 'widgets-2-style' );
-		wp_enqueue_style( 'mag-popup' );
-	}
+    function enqueue_elementor_widget_styles() {
+        wp_enqueue_style( 'widgets-2-style' );
+        wp_enqueue_style( 'mag-popup' );
+    }
 
-	function register_widget_scripts() {
-		wp_register_script( 'jquery-popup', plugins_url( '/assets/Popup/lib/jquery.js', __FILE__ ) );
-		wp_register_script( 'widgets-2-script', plugins_url( '/assets/js/widgets-2-script.js', __FILE__ ) );
-		wp_register_script( 'popup-min', plugins_url( '/assets/Popup/lib/jquery.magnific-popup.min.js', __FILE__ ) );
-		
-	}
-	
-	
+    function register_widget_scripts() {
+        wp_register_script( 'jquery-popup', plugins_url( '/assets/Popup/lib/jquery.js', __FILE__ ) );
+        wp_register_script( 'widgets-2-script', plugins_url( '/assets/js/widgets-2-script.js', __FILE__ ) );
+        wp_register_script( 'popup-min', plugins_url( '/assets/Popup/lib/jquery.magnific-popup.min.js', __FILE__ ) );
+
+    }
+
+
 
 }
-
